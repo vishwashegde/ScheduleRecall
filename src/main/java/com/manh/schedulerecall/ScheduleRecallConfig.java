89a78d649e93dada0f3e74e910290fa6c90a7d37
Vishwas Hegde <vihegde@manh.com>
2024-02-14 19:40:32 +0530

    Added Eureka client registry

--- src/main/java/com/manh/schedulerecall/ScheduleRecallConfig.java
+++ src/main/java/com/manh/schedulerecall/ScheduleRecallConfig.java
@@ -1,10 +1,12 @@
 package com.manh.schedulerecall;
 
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @EnableScheduling
+@EnableEurekaClient
 public class ScheduleRecallConfig {
 
 }
